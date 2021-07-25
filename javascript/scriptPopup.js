8194d85b6ea77debafb8ea6d9ac17611e4447d0a
Sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2021-07-24 19:46:17 -0500

    Se agrego la seccion de videos de youtube

--- javascript/scriptPopup.js
+++ javascript/scriptPopup.js
@@ -1,29 +1,6 @@
-const abrirCuadro = document.querySelectorAll('[data-modal-target]')
-const cerrarCuadro = document.querySelectorAll('[data-close-button]')
-const cubrir = document.getElementById('cubrir')
-
-abrirCuadro.forEach(button => {
-    button.addEventListener('click',() => {
-        const cuadro = document.querySelector(button.dataset.modalTarget)
-        abrir(cuadro)
-    })
-})
-
-function abrir(cuadro){
-    if(cuadro == null) return
-    cuadro.classList.add('activo')
-    cubrir.classList.add('activo')
-}
-
-cerrarCuadro.forEach(button => {
-    button.addEventListener('click', () => {
-        const cuadro = button.closest('.cuadro')
-        cerrar(cuadro)
-    })
-})
-
-function cerrar(cuadro){
-    if(cuadro == null) return
-    cuadro.classList.remove('activo')
-    cubrir.classList.remove('activo')
+function toggle(){
+    var blur = document.getElementById('popup')
+    blur.classList.toggle('active')
+    var blur = document.getElementById('cuadro')
+    blur.classList.toggle('active')
 }
