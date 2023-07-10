d23df6f24b6ccf50cba40c1780097003e88dd499
Sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2023-07-09 18:43:55 -0600

    Added countdown

---
+++ javascript/countdown.js
@@ -0,0 +1,29 @@
+var countDownDate = new Date("July 12, 2023 17:00:00").getTime();
+
+var countdownInterval = setInterval(function() {
+var now = new Date().getTime();
+
+var timeRemaining = countDownDate - now;
+
+var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+var hours = Math.floor(
+(timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+);
+var minutes = Math.floor(
+(timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+);
+var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+if (timeRemaining < 0) {
+clearInterval(countdownInterval);
+document.getElementById("days").innerHTML = "0";
+document.getElementById("hours").innerHTML = "0";
+document.getElementById("minutes").innerHTML = "0";
+document.getElementById("seconds").innerHTML = "0";
+}
+}, 1000);
