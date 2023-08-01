9de827335373b226072db1fadfa04d18724ff4dd
Sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2023-08-01 01:49:34 -0600

    Fixed my mistakes, sorry

--- javascript/scriptPopup.js
+++ javascript/scriptPopup.js
@@ -1,10 +1,22 @@
-function toggle(){
+function toggle(box){
     var blur = document.getElementById('popup')
-    blur.classList.toggle('active')
-    var blur = document.getElementById('box')
     blur.classList.toggle('active')
     var blurElements = document.getElementsByClassName('blur');
         for (var i = 0; i < blurElements.length; i++) {
             blurElements[i].classList.toggle('active');
         }
+    console.log('Que cojones',box)
+    if(box=='box-1'){
+        var blur = document.getElementById('box-1')
+        console.log('111111111111')
+    }
+    if(box=='box-2'){
+        var blur = document.getElementById('box-2')
+        console.log('222222222222222')
+    } 
+    if(box=='box-3'){
+        var blur = document.getElementById('box-3')
+    }     
+    blur.classList.toggle('active')
+
 }
